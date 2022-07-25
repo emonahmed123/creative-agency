8e404fc5c3a8dc63b32969e459643d0027b4d10f
emonahmed123 <imonshomon@gmail.com>
2022-07-25 16:36:39 +0600

    added home section

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import fram from '../../../images/logos/Frame.png'
+const Banner = () => {
+  return (
+    <div>
+   <div class="hero min-h-screen shrink-0">
+  <div class="hero-content flex-col lg:flex-row-reverse gap-32">
+    <img src={fram} class="max-w-lg  rounded-lg" />
+    <div className=''>
+      <h1 class="text-5xl font-bold">Let’s Grow Your <br/>
+         Brand To The <br/>
+         Next Level</h1>
+      <p class="py-6">Lorem ipsum dolor sit amet, consectetur<br/> adipiscing elit. Purus commodo ipsum duis <br/> laoreet maecenas. Feugiat </p>
+      <button className="btn btn-active w-40 h-11">Hire Us</button>
+
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../SharedPage/Navbar'
+import Banner from './Banner/Banner'
+import './Home.css'
+const Home=()=>{
+
+ return(
+     <div className='bg bg-red-400 Home'>
+      <Navbar></Navbar>
+      <Banner></Banner>
+     </div>
+
+
+ )
+
+
+
+}
+  export default Home
